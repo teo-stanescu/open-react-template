836daf5c54a7a11b6a3f7d7b1745ec81a1e55a57
Teo Stanescu <teodor.stanescu@okapihq.com>
2021-07-22 20:00:00 +0300

    change logo and colors

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -61,11 +61,11 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              Landing template for <span className="text-color-primary">startups</span>
+              Flip your website into a <span className="text-color-primary">mobile app</span>
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-                Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
+                Thousands of companies invest milions in order to create mobile apps. Are you looking for a simpler solution?
                 </p>
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
